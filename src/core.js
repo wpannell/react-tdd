9767fc3d867164f6ca57a429447289a0f15674a4
WIL PANNELL <wil.pannell@gmail.com>
2015-09-27 05:06:53 -0700

    refactor for legibility

--- src/core.js
+++ src/core.js
@@ -1,3 +1,5 @@
+import {List} from 'immutable';
+
 export function loadEntriesInto(state, entries) {
-  return state.set('entries', entries);
+  return state.set('entries', new List(entries));
 }
--- test/core-spec.js
+++ test/core-spec.js
@@ -2,16 +2,33 @@ import {expect} from 'chai';
 import {List, Map} from 'immutable';
 import {loadEntriesInto} from '../src/core';
 
-describe('application logic', () => {
-  describe('load entries', () => {
+describe('core logic', () => {
+  describe('load entries produces new state ', () => {
 
-    it('produces new state from immutable', () => {
+    it('from immutable', () => {
       const state = new Map();
 
       const entries = (List.of(
         'trainspotting',
         '28 days later'
       ));
+
+      const nextState = loadEntriesInto(state, entries);
+
+      expect(nextState).to.equal(new Map({
+        entries: List.of(
+          'trainspotting',
+          '28 days later'
+        )}));
+    });
+
+    it('from iterable', () => {
+      const state = new Map();
+
+      const entries = [
+        'trainspotting',
+        '28 days later'
+      ];
 
       const nextState = loadEntriesInto(state, entries);
 
