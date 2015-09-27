37a929073928ef5940be12bf05d09880e6ed718c
WIL PANNELL <wil.pannell@gmail.com>
2015-09-27 05:07:13 -0700

    introduce next

--- test/core-spec.js
+++ test/core-spec.js
@@ -3,6 +3,7 @@ import {List, Map} from 'immutable';
 import {loadEntriesInto} from '../src/core';
 
 describe('core logic', () => {
+
   describe('load entries produces new state ', () => {
 
     it('from immutable', () => {
@@ -37,6 +38,36 @@ describe('core logic', () => {
           'trainspotting',
           '28 days later'
         )}));
+    });
+  });
+
+  describe('next', () => {
+
+    function next(state) {
+      const entries = state.get('entries');
+      return state.merge({
+        vote: Map({pair: entries.take(2)}),
+        entries: entries.skip(2)
+      });
+    }
+
+    it('takes the next 2 entries on which to vote', () => {
+      const state = new Map({
+        entries: List.of(
+          'trainspotting',
+          '28 days later',
+          'sunshine'
+        )
+      });
+
+      const nextState = next(state);
+
+      expect(nextState).to.equal(new Map({
+        vote: Map({
+          pair: List.of('trainspotting', '28 days later')
+        }),
+        entries: List.of('sunshine')
+      }));
     });
   });
 });
