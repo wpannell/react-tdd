5d2ccdb4a350a82d849fe495f9e4530c94e1c818
WIL PANNELL <wil.pannell@gmail.com>
2015-09-27 19:12:24 -0700

    introduce reduce

---
+++ test/reduce-spec.js
@@ -0,0 +1,18 @@
+import {expect} from 'chai';
+import {fromJS, Map} from 'immutable';
+import {loadMoviesInto} from '../src/core';
+function reduce(state, action) {
+  return loadMoviesInto(state, action.movies);
+}
+
+describe('reduce', () => {
+  it.only('handles LOAD_MOVIES', () => {
+    const firstState = Map();
+    const action = {type: 'LOAD_MOVIES', movies: ['trainspotting']};
+    const nextState = reduce(firstState, action);
+
+    expect(nextState).to.equal(
+      fromJS({movies: ['trainspotting']})
+    );
+  });
+});
